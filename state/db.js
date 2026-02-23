a3b7da2d6658cdae81bb25b884c055bda38b2206
alpinesystem <ellite@ellite.dev>
2026-02-23 22:16:42 +0100

    add test database and autimatically handle stuff

--- state/db.js
+++ state/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-	const uri = process.env.MONGODB_URI;
-	if (!uri) throw new Error('MONGODB_URI is not set in .env');
+	const uri = process.env.NODE_ENV === 'development'
+		? process.env.MONGODB_TEST_URI
+		: process.env.MONGODB_URI;
+	if (!uri) throw new Error('MongoDB URI is not set in .env');
 	await mongoose.connect(uri);
-	console.log('Connected to MongoDB');
+	const dbType = process.env.NODE_ENV === 'development' ? 'test' : 'production';
+	console.log(`Connected to MongoDB (${dbType})`);
 }
 
 module.exports = { connectDB };
